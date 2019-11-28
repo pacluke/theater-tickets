688fa90c4ba1ee1a16f3fbcd096ce241b876b5d5
pacluke <lucasflores77@gmail.com>
2019-11-27 22:14:28 -0300

    Initial project

---
+++ theater-tickets/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theater-tickets
+//
+//  Created by Lucas Flores on 27/11/19.
+//  Copyright © 2019 lsflores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ theater-ticketsTests/theater_ticketsTests.swift
@@ -0,0 +1,34 @@
+//
+//  theater_ticketsTests.swift
+//  theater-ticketsTests
+//
+//  Created by Lucas Flores on 27/11/19.
+//  Copyright © 2019 lsflores. All rights reserved.
+//
+
+import XCTest
+@testable import theater_tickets
+
+class theater_ticketsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
