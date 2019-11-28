035b82c0876b25560524201cacb987a81283b6a4
pacluke <lucasflores77@gmail.com>
2019-11-27 22:40:38 -0300

    Weekdays enum

---
+++ theater-tickets/Model/Weekday.swift
@@ -0,0 +1,19 @@
+//
+//  Weekday.swift
+//  theater-tickets
+//
+//  Created by Lucas Flores on 27/11/19.
+//  Copyright © 2019 lsflores. All rights reserved.
+//
+
+import Foundation
+
+enum Weekday {
+    case Sunday
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+}
