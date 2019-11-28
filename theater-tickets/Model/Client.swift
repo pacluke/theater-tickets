4c903af48ae05f5b84829d024b472d26f57f7203
pacluke <lucasflores77@gmail.com>
2019-11-27 22:36:54 -0300

    Types of client

---
+++ theater-tickets/Model/Client.swift
@@ -0,0 +1,15 @@
+//
+//  Client.swift
+//  theater-tickets
+//
+//  Created by Lucas Flores on 27/11/19.
+//  Copyright © 2019 lsflores. All rights reserved.
+//
+
+import Foundation
+
+enum Client {
+    case Kid
+    case Student
+    case Elder
+}
