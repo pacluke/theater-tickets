540c36adc630103028a944ec47b0f3e3ebd7c03c
pacluke <lucasflores77@gmail.com>
2019-11-28 22:49:32 -0300

    Confirmed student ticket without business rules

---
+++ theater-tickets/Model/Ticket/ConfirmedStudentTicket.swift
@@ -0,0 +1,44 @@
+//
+//  ConfirmedStudentTicket.swift
+//  theater-tickets
+//
+//  Created by Lucas Flores on 28/11/19.
+//  Copyright © 2019 lsflores. All rights reserved.
+//
+
+import Foundation
+
+struct ConfirmedStudentTicket: TicketProtocol {
+    var price: Float
+    var priceWithDiscount: Float!
+    var client: Client
+    var weekday: Weekday
+    
+    init(price: Float, weekday: Weekday) {
+        self.price = price
+        self.weekday = weekday
+        self.client = .Elder
+        self.priceWithDiscount = price - (getDiscount(weekday: weekday) * price)
+    }
+    
+    func getDiscount(weekday: Weekday) -> Float {
+        switch weekday {
+        case .Sunday:
+            return 0.0
+        case .Monday:
+            return 0.0
+        case .Tuesday:
+            return 0.0
+        case .Wednesday:
+            return 0.0
+        case .Thursday:
+            return 0.0
+        case .Friday:
+            return 0.0
+        case .Saturday:
+            return 0.0
+        case .Holiday:
+            return 0.0
+        }
+    }
+}
