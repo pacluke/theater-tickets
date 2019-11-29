8a397ada2773b4078c061c924e2ce75287c218d8
pacluke <lucasflores77@gmail.com>
2019-11-28 22:44:56 -0300

    Change ticket protocol so it returns the discount based on the weekday

--- theater-tickets/Protocol/TicketProtocol.swift
+++ theater-tickets/Protocol/TicketProtocol.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 protocol TicketProtocol {
-    func mondayDiscount() -> Float
-    func tuesdayDiscount() -> Float
-    func wednesdayDiscount() -> Float
-    func thursdayDiscount() -> Float
-    func fridayDiscount() -> Float
-    func saturdayDiscount() -> Float
-    func sundayDiscount() -> Float
-    func holidayDiscount() -> Float
+    
+    var price: Float { get }
+    var priceWithDiscount: Float! { get }
+    var client: Client { get }
+    var weekday: Weekday { get }
+    
+    func getDiscount(weekday: Weekday) -> Float
+
 }
